1cd0b60c6d0cd519e0cfaed40a07051a466f0ea2
Josephine <sosodjibrildiop@gmail.com>
2022-11-29 18:46:15 +0000

    send

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageConnexionComponent } from './page-connexion/page-connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageConnexionComponent
   ],
   imports: [
     BrowserModule
