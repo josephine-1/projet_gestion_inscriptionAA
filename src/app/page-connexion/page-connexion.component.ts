732c273e06b30a4786206fd916ab5b969a9af0fc
Josephine <sosodjibrildiop@gmail.com>
2022-12-02 15:35:19 +0000

    cc

--- src/app/page-connexion/page-connexion.component.ts
+++ src/app/page-connexion/page-connexion.component.ts
@@ -7,21 +7,34 @@ import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 })
 export class PageConnexionComponent {
   angForm!: FormGroup;
+  submitted = false;
  
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
   createForm() {
     this.angForm = this.fb.group({
-       email: ['', Validators.required],
-       password:['', Validators.required]
+      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: ['', [Validators.required/* , Validators.minLength(6) */]]
+    
     });
   }
 
+
+  
   /* constructor(private formBuilder :FormBuilder) {} */
   ngOnInit(){
     //validation
+    this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.angForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.angForm.value))
+    }
    
   }
-}
+
 
