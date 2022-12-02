71e61471a7457c8f5ce7fbd0d6253ccb65dc1e85
josephine-1 <102295400+josephine-1@users.noreply.github.com>
2022-12-02 17:26:39 +0000

    Mhd lamine (#4)
    * front finish
    
    * le routing
    
    * front
    
    Co-authored-by: MHDLamine <mouhamed.ngom50@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularPaginatorModule } from 'angular-paginator';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { TableauAdmComponent } from './tableau-adm/tableau-adm.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { AppRoutingModule } from './app-routing.module';
+import { TableauAdmArchiveComponent } from './tableau-adm-archive/tableau-adm-archive.component';
+import { ModifierComponent } from './modifier/modifier.component';
+
+
+
 import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+
+    NavbarComponent,
+    TableauAdmComponent,
+    TableauAdmArchiveComponent,
+    ModifierComponent
+  ],
+  imports: [  
+    ReactiveFormsModule,
+    AngularPaginatorModule,
+    BrowserModule,
+    NgxPaginationModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    AppRoutingModule
+
+
     InscriptionComponent
   ],
   imports: [
@@ -17,8 +45,11 @@ import { InscriptionComponent } from './inscription/inscription.component';
     FormsModule,
     ReactiveFormsModule,
     
+
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
+
