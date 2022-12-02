e00e9060819bec1bd3d490de74deb47d9d188506
koumba6 <diongue901@gmail.com>
2022-12-02 16:50:48 +0000

    angular

---
+++ src/app/model/inscription.ts
@@ -0,0 +1,13 @@
+export class inscription {
+
+    constructor(
+        public nom: string,
+        public prenom: string,
+        public email: string,
+        public role: string,
+        public motdepasse: string,
+        public confirmation: string,
+        
+
+    ) { }
+}
