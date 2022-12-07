e0b244ac140fad8a4d4a236bd79c427c6c0bf75e
MHDLamine <mouhamed.ngom50@gmail.com>
2022-12-07 15:48:46 +0000

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { PageUserComponent } from './page-user/page-user.component';
 import { PageConnexionComponent } from './page-connexion/page-connexion.component';
 import { AngularPaginatorModule } from 'angular-paginator';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 
 import { TableauAdmComponent } from './tableau-adm/tableau-adm.component';
+=======
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+>>>>>>> MHDLamine
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AppRoutingModule } from './app-routing.module';
 import { TableauAdmArchiveComponent } from './tableau-adm-archive/tableau-adm-archive.component';
 import { ModifierComponent } from './modifier/modifier.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TableauAdmComponent } from './tableau-adm/tableau-adm.component';
+
 
 
 @NgModule({
   declarations:
   [
-
+    TableauAdmComponent,
+    PageUserComponent,
     NavbarComponent,
     AppComponent,
     InscriptionComponent,
     ModifierComponent,
     PageConnexionComponent,
     PageUserComponent,
-    TableauAdmComponent,
-    TableauAdmArchiveComponent
+    TableauAdmArchiveComponent,
+
   ],
   imports: [
     Ng2SearchPipeModule,
